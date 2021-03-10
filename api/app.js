3568650fb4549d3281acb172159feeb1bb332923
berkansivri <berkansivri@gmail.com>
2021-03-10 18:44:56 +0300

    added cors package

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 require('dotenv').config();
 
 const productsRouter = require('./routes/products');
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/products', productsRouter);
 
